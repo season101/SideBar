829458dabdd2768d432dc069a1b3972d6fe3a551
Sijan Malla <sijan.malla101@gmail.com>
2023-09-16 15:00:27 -0500

    Added the functionality for close-open sidebar/modal

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 import { links, social } from './data';
 import logo from './logo.svg';
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <aside className="sidebar show-sidebar">
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className="logo" />
-        <button className="close-btn">
+        <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
